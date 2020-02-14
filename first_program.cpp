a94c904851e059e1956c80bb5187383e8baed18f
M1NH42 <minhazalam365@gmail.com>
2020-02-15 00:59:48 +0530

    First program

---
+++ first_program.cpp
@@ -0,0 +1,18 @@
+/** This program prints the hello world message
+ * It's a first program that we learn in any 
+ * language
+ */
+
+//  include header file
+#include<iostream>
+
+//  using this to avoid typing std again and again
+using namespace std;
+
+//  entry point of a program
+int main()
+{
+    cout << "Hello world" << endl;
+
+    return 0; //beacause the main fxn is of int type
+}
