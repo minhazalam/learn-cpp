5b5ec5b0b83f810c897d340d1f51a9c9cc373855
M1NH42 <minhazalam365@gmail.com>
2020-02-16 00:30:32 +0530

    Added inc dec program

--- inc_dec_op.cpp
+++ inc_dec_op.cpp
@@ -2,3 +2,21 @@
 //  there are two types of inc and dec operators
 //  a) pre
 //  b) post
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+    cin >> i >> k; // 4, 5
+
+    j = i++ + k;
+    cout << j << endl; //9
+
+    j = i + k;
+    cout << j << endl; // 10
+
+    return 0;
+}
