3a39d8c2ee92dbb2ffe1e0f77ec340058a697a1e
M1NH42 <minhazalam365@gmail.com>
2020-02-16 01:24:46 +0530

    Added typedef program

---
+++ typedef.cpp
@@ -0,0 +1,19 @@
+// typedef demonstrator program
+
+#include<iostream>
+using namespace std;
+
+typedef string name;
+typedef int roll_no;
+typedef int marks;
+
+int main()
+{
+    name n1, n2, n3;
+    roll_no r1, r2;
+    marks m1, m2, m3;
+}
+
+/* typedef actually increases the readadbility of the program
+   As most of us dont use meaningful names in hurry so it will
+   take care that we dont forget what is r1, m1 and n1 */
