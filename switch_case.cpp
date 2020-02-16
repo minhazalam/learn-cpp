7a3d066870d5fa9c420fa0f3f597ed6118512595
M1NH42 <minhazalam365@gmail.com>
2020-02-16 16:33:55 +0530

    Switch case program

---
+++ switch_case.cpp
@@ -0,0 +1,47 @@
+// program shows working of switch case
+// used to write menu driven program
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter day number : ";
+    
+    int day;
+    cin >> day;
+
+    switch (day)
+    {
+        case 1:
+            cout <<"Monday\n";
+            break;
+        case 2:
+        cout << "tues";
+        break;
+
+        case 3:
+        cout << "wed";
+        break;
+
+        case 4:
+        cout << "thurs";
+        break;
+
+        case 5:
+        cout <<"fri\n";
+        break;
+
+        case 6:
+        cout << "sat\n";
+        break;
+
+        case 7:
+        cout << "sunday\n";
+        break;
+
+        default:
+        cout << "Invalid day\n";
+    }
+    return 0;
+}
