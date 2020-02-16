b07a2e95cac855e60b18a61f88cc6b8b87a5b961
M1NH42 <minhazalam365@gmail.com>
2020-02-16 19:04:30 +0530

    Added do..while program

--- loops/while_loop.cpp
+++ loops/while_loop.cpp
@@ -11,10 +11,21 @@ int main()
 
     int i = 2;
 
-    while (i <= n)
+    /* while (i <= n)
     {
         cout << i << endl;
         i += 2;
-    }
+    } */
+
+    // do.. while loop
+
+    do
+    {
+        /* code */
+        cout << i << "\n";
+        i += 2;
+    } while (i <= n);
+    
+
     return 0;
 }
