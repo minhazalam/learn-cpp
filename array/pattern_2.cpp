ec0d2fcb45fe0754ef33ec71afafbcb5c89e9317
M1NH42 <minhazalam365@gmail.com>
2020-02-17 17:58:53 +0530

    Star pattern

---
+++ array/pattern_2.cpp
@@ -0,0 +1,21 @@
+// star pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i =0;i < 4; i++)
+    {
+        for (int j=0; j < 4; j++)
+        {
+            if (i >= j)
+            {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
