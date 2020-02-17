17f702de55060398424cebe7a57a54ab5f3130a5
M1NH42 <minhazalam365@gmail.com>
2020-02-17 22:43:09 +0530

    Pointer to a function

---
+++ pointer/pointer_to_func.cpp
@@ -0,0 +1,20 @@
+// pointer to a function
+
+#include<iostream>
+using namespace std;
+
+void display()
+{
+    cout << "Minhaz" << endl;
+}
+
+int main()
+{
+    void (*fp)(); // declaration
+
+    fp = display; // initialise
+
+    fp (); // called
+
+    return 0;
+}
