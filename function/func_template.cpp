3549f7e71cd69e1ea18655a2cec8d1771fbedd29
M1NH42 <minhazalam365@gmail.com>
2020-02-18 14:08:04 +0530

    Function template

---
+++ function/func_template.cpp
@@ -0,0 +1,33 @@
+// program shows the working of function template
+
+#include<iostream>
+using namespace std;
+
+//declaration of func template
+template <class T>
+
+T maxim (T a, T b) // func of template class declared
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+    
+}
+
+int main()
+{
+
+    int i = maxim (10, 15); // int type called
+    float f = maxim (12.3f, 21.6f); // float type called
+
+    cout<< "Max : " << i << endl;
+
+    cout << "Max : " << f << endl;
+
+    return 0;
+}
