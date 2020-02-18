10b54a1ed0032e6bbcfe8d31c899792be4d6f005
M1NH42 <minhazalam365@gmail.com>
2020-02-18 17:43:25 +0530

    Pass by address

---
+++ function/call_by_add.cpp
@@ -0,0 +1,29 @@
+// program shows the working of call by address
+// by using pointers
+
+#include<iostream>
+using namespace std;
+
+// func to swap two numbers
+// pointers a & b stores addresses of x and y
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp= *a;
+    *a= *b;
+    *b = temp;
+}
+
+int main()
+{
+    int x, y;
+    cout << "Enter vlues of x & y : ";
+    cin >>x>>y;
+
+    swap(&x, &y);
+
+    cout << "x : " << x << " "<< "y " <<y<<endl;
+
+    return 0;
+}
